d3dcfb0a20681c42ce48f77fba52bd583daadc51
siddarthsingotam <siddarthsingotam1213@gmail.com>
2024-08-23 10:29:12 +0300

    Sample init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,31 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // println("Program arguments: ${args.joinToString()}")
 
-    printSomething()
+    reversedFormat("Sandals")
 }
 
-fun printSomething(){
+fun printSomething() {
     println("Hello there")
 
     var sum = 0
-    for (i in 1..6){
+    for (i in 1..6) {
         sum += i
         println("number $i= $sum")
     }
 }
+
+fun compare(a: Int, b: Int) {
+    if (a >= b) {
+        println(" $a is Greater than $b ")
+    } else {
+        println("$a not greater than $b")
+    }
+}
+
+fun reversedFormat(sentence: String) {
+    println(sentence.reversed())
+}
+
